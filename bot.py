e5424dcfdfcb4772bd28767111e17ccb7a6783e8
Modernszz <yudisuryana1967@gmail.com>
2026-08-16 21:19:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
+import random
 import discord
+from kamus import daftar_kamus
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,7 +17,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower() == '!sunda':
+    # Pariksa lamun pesen diawali ku !sw
+    if message.content.lower().startswith('!sw'):
+        kata_kunci = message.content[3:].strip()
+        
+        if not kata_kunci:
+            await message.channel.send("Naon nu rek ditéang dina Kamus Gaul Sunda? Contoh: `!sw ganteng` atanapi `!sw budak baong`")
+            return
+
+        # Milih definisi roasting sacara acak tina file kamus.py
+        arti_gaul = random.choice(daftar_kamus)
+        
+        # Format balasan bot
+        balasan = f"📖 **Kamus Gaul Sunda (Edisi Roasting):**\n> **{kata_kunci.upper()}** *(n)*\n> Artinya: {arti_gaul}"
+        await message.channel.send(balasan)
+
+    elif message.content.lower() == '!sunda':
         await message.channel.send('Wilujeng sumping di server Sunda, Lur! ☕ Mejeh Euy!')
 
 client.run(os.getenv('DISCORD_TOKEN'))
