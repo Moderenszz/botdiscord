38838865326ce1446684773601383c683875f971
Modernszz <yudisuryana1967@gmail.com>
2026-08-16 21:18:58 +0700

    Create kamus.py

---
+++ kamus.py
@@ -0,0 +1,14 @@
+# Daptar Kamus Gaul Sunda / Roasting (Bisa ditambahkeun ku anjeun nepi ka ratusan)
+daftar_kamus = [
+    "makhluk lemes nu boga hulu tapi teu boga uteuk, alias tolol permanén.",
+    "jelema nu resep nginjeum duit tapi poho mulangkeun, pas ditagih malah ngablokir.",
+    "jelema nu ngaku-ngaku pinter tur kasep, padahal mun ningali kaca ogé eunteungna ménta ampun.",
+    "spésiés manusa nu hirupna ngan saukur numpang toél jeung jadi beban kolot.",
+    "ganteng ti mana anjir? Katingali ti gigir siga baskom pecah.",
+    "sok ngaca heula sateuacan ngomongkeun kasep, sieun eunteungna nu menta hampura.",
+    "maneh mah kasep lamun lampu kamar dipareuman kabeh.",
+    "embung gawe tapi hayang loba duit, gaya hirup siga sultan tapi dompet eusi salembar duit lecek.",
+    "sok ngagaya siga budak hit, padahal meuli kuota gé menta ka indungna.",
+    "jelema nu resep gogorowokan di jalan padahal mun panggih jeung maung langsung ngompol di calana.",
+    # Silakan tambahkeun deui di dieu nepi ka 100 kalimah atawa leuwih ku anjeun!
+]
